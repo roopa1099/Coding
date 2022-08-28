253cd5c51f7d37f66efe15836014d97fd5a11c42
Roopa R <roopar710@gmail.com>
2022-08-28 13:15:49 +0530

    DSA Array Property practice 28-Aug

--- PlacementCoding/src/com/coding/Startclass.java
+++ PlacementCoding/src/com/coding/Startclass.java
@@ -1,2 +1,10 @@
-package com.coding;public class Startclass {
+package com.coding;
+
+public class Startclass {
+
+        public static void main(String[] args){
+            System.out.println("Hello Success");
+        }
+
+
 }
