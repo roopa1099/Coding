94434e7d872253b9947d5403951095f1c58f3b07
Roopa R <roopar710@gmail.com>
2022-09-14 11:26:27 +0530

    Educate Sliding Window-7 if you are allowed to replace no more than k letters with any 0s, find the length of the longest substring having max 1 after replacement.

---
+++ PlacementCoding/src/com/coding/LongestSubarrayReplaced0With1.java
@@ -0,0 +1,38 @@
+package com.coding;
+
+/**
+ * Given an array containing 0s and 1s, if you are allowed to replace no more than ‘k’ 0s with 1s, find the length of the longest contiguous subarray having all 1s.
+ *
+ * Example 1:
+ *
+ * Input: Array=[0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1], k=2
+ * Output: 6
+ * Explanation: Replace the '0' at index 5 and 8 to have the longest contiguous subarray of 1s having length 6.
+ */
+public class LongestSubarrayReplaced0With1 {
+    public static int findLength(int[] arr, int k) {
+        // TODO: Write your code here
+        int one=0,start=0,maxLength=0,tempLength;
+        for(int i=0;i<arr.length;i++){
+            int current=arr[i];
+            if(current==1)
+                one++;
+            // rest 0's in subarray can it be replaced within k
+            if(((i-start+1)-one)>k){
+                if(arr[start]==1)
+                    one--;
+                start++;
+            }
+
+            tempLength=i-start+1;
+            maxLength=Math.max(maxLength,tempLength);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args){
+        int[] array={0, 1, 1, 0, 0, 0, 1, 1, 0, 1, 1};
+        int val=findLength(array,1);
+        System.out.println(val);
+    }
+}
