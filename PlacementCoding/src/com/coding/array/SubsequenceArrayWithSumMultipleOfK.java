ef534e4be60f74576be624aa6866296d13e29732
Roopa R <64922182+roopa1099@users.noreply.github.com>
2023-02-27 00:38:37 +0530

    Update SubsequenceArrayWithSumMultipleOfK.java

--- PlacementCoding/src/com/coding/array/SubsequenceArrayWithSumMultipleOfK.java
+++ PlacementCoding/src/com/coding/array/SubsequenceArrayWithSumMultipleOfK.java
@@ -8,7 +8,7 @@ import java.util.Map;
  *
  * An integer x is a multiple of k if there exists an integer n such that x = n * k. 0 is always a multiple of k.
  *
- *
+ * Min elements in subarray must be 2
  *
  * Example 1:
  *
