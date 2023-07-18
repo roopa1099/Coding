a1249de60edc19e05db9b00f6cc5115786aa3543
RoopaRavindranath <roopar@fico.com>
2023-07-18 21:59:21 +0530

    fix decimal to binary

--- PlacementCoding/src/com/coding/array/DecimalToBinary.java
+++ PlacementCoding/src/com/coding/array/DecimalToBinary.java
@@ -1,22 +1,22 @@
 package com.coding.array;
+
 //check
 public class DecimalToBinary {
 
-    public static double binary(int number){
-        double pow=0,result=0;
-        while (number>0){
-            int rem=number%2;
-            number=number/2;
-            result=Math.pow(10,pow)+rem;
+    public static double binary(int number) {
+        double result = 0, pow = 0;
+        while (number > 0) {
+            int rem = number % 2;
+            number = number / 2;
+            result = Math.pow(10, pow) * rem + result;
             pow++;
         }
 
-        return result;
+        return (int) result;
     }
 
-    public static void main(String[] args){
-        int num=15;
+    public static void main(String[] args) {
+        int num = 3;
         System.out.println(binary(num));
     }
 }
-
